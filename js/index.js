f0c23548ad9784c2607a2e1fd1ea25c043d6215e
BingDarla <hbshuang@gmail.com>
2018-01-21 22:30:39 +1100

    some basic function done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,94 @@
-$(document).ready(function(){
-  chords.gridTable();
+$(document).ready(function() {
+  console.log('The document is ready');
+  ChordMap.gridTable();
+  let drumIsSelected = false;
+  let pianoIsSelected = false;
+
+
+  $('.tone').click(function() {
+    let id = $(this).attr('id');
+    let i = id.split(' ')[0];
+    let j = id.split(' ')[1];
+    //check if this tone is selected or not
+    if (ChordMap.chordsmap[i][j] == 1) {
+      $(this).removeClass('selected');
+      ChordMap.chordsmap[i][j] = 0;
+    } else {
+      $(this).addClass('selected');
+      ChordMap.chordsmap[i][j] = 1;
+    }
+
+    var synth = new Tone.Synth().toMaster()
+    val = $(this).text();
+    tone = '"' + val + '"';
+    // console.log(tone);
+    if (pianoIsSelected) {
+      synthNode(val);
+    }
+    if (drumIsSelected) {
+      drumNode(val);
+      console.log('drum');
+    }
+
+
+    console.log(ChordMap.chordsmap);
+
+
+
+  });
+  $('#piano').click(function() {
+    if (!pianoIsSelected) {
+      $('#piano').addClass('choose');
+      pianoIsSelected = true;
+
+    } else {
+      $('#piano').removeClass('choose');
+      pianoIsSelected = false;
+    }
+  });
+  $('#drum').click(function() {
+    if (!drumIsSelected) {
+      $('#drum').addClass('choose');
+      drumIsSelected = true;
+
+    } else {
+      $('#drum').removeClass('choose');
+      drumIsSelected = false;
+    }
+  });
+  $('#save').click(function(){
+      if (pianoIsSelected){
+        ChordMap.pianoPlayList=[];
+        ChordMap.pianoPlayList=ChordMap.toneMap();
+      }
+      if (drumIsSelected){
+        ChordMap.drumPlayList=[];
+        ChordMap.drumPlayList=ChordMap.toneMap();
+
+      }
+
+  });
+  $('#play').click(function() {
+
+    // console.log(ChordMap.pianoPlayList);
+    ChordMap.pianoPlayList.forEach((tone, index) => {
+      setTimeout(() => {
+
+        poly(tone.length, tone);
+      }, 500 * index);
+    });
+    ChordMap.drumPlayList.forEach((tone, index) => {
+      setTimeout(() => {
+
+
+      }, 500 * index);
+    });
+    // setTimeout(() => {
+    // }, 500 * 17);
+  });
+
+  $('#clear').click(function(){
+    ChordMap.reset();
+
+  })
 });
--- js/player.js
+++ js/player.js
@@ -1,17 +1,123 @@
-const chordMap={
-  //Each coloum is one note array
-  let chord:[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  const  chords = ['C5','B4','A4','G4','F4','E4','D4','C4','B3','A5','G3','F3','E3','D3','C2'];
-  const m:16,
+const ChordMap = {
+  //Each row is one note array[C5...C2] multichoice
+  //15 synth
+
+  chordsmap: [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ],
+  // chordsmap 14 * 15
+  CHORDS: ['C5', 'B4', 'A4', 'G4', 'F4', 'E4', 'D4', 'C4', 'B3', 'A5', 'G3', 'F3', 'E3', 'D3', 'C2'],
+  pianoPlayList: [],
+  drumPLayList: [],
+
   //function to generate the whole gridTable
-  const gridTable:function(m){
-    for (let i=1,i<16;i++){
-      let $r = '<div></div>';
-      for (let j=1; j<14;j++){
-        $col = '<div></div>';
-        $r.append($col);
+  gridTable: function() {
+    for (let i = 0; i < 14; i++) {
+
+      let name = this.CHORDS[i];
+      let $row = $('<div>');
+      $row.attr('class', 'row');
+      $('#play-table').append($row);
+      for (let j = 0; j < 15; j++) {
+        $col = $('<div>');
+        let id = j.toString() + ' ' + i.toString();
+        $col.attr('id', id).attr('class', 'tone');
+        $col.text(name);
+        $row.append($col);
       }
-      $('#container').append($r);
     }
+  },
+  //generatin toneplaylist array
+  toneMap: function() {
+    let playList = [];
+    console.log('tonemap is called');
+    console.log(this.chordsmap);
+    for (let i = 0; i < 14; i++) {
+      let chordsA = this.chordsmap[i];
+      console.log(chordsA);
+      let tone = [];
+      for (let j = 0; j < chordsA.length; j++) {
+        if (chordsA[j] == 1) {
+          tone.push(this.CHORDS[j]);
+        }
+      }
+      console.log(`tone is ${tone}`);
+      playList.push(tone);
+    }
+    console.log(playList);
+    return playList;
+  },
+  //clear all the selected tones;
+  reset: function() {
+    this.pianoPlayList = [];
+    this.drumPLayList = [];
+    this.chordsmap = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    $('.tone').removeClass('selected');
   }
+};
+
+const synthNode = function(note) {
+  let synth = new Tone.Synth().toMaster();
+  synth.triggerAttackRelease(note, '16n');
+};
+
+const drumNode = function(note) {
+  let synth = new Tone.MembraneSynth().toMaster();
+  synth.triggerAttackRelease(note, "8n");
+  console.log('drum is called');
+}
+
+
+
+const t1 = function(note) {
+
+  let synth = new Tone.Oscillator(440, "sine").toMaster() ;
+  synth.triggerAttackRelease(note, "2n");
+  console.log('drum is called');
+}
+
+
+
+
+
+
+
+const poly = function(tones, chordArray) {
+  let polySynth = new Tone.PolySynth(tones, Tone.Synth).toMaster();
+  polySynth.triggerAttackRelease(chordArray, '16n');
+};
+
+const drum = function(tones, chordArray) {
+  let polySynth = new Tone.MembraneSynth(tones, Tone.Synth).toMaster();
+  polySynth.triggerAttackRelease(chordArray, '16n');
 }
